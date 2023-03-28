026849c0ffbc2f11b22fe381c4204db3d129e0c9
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-28 06:31:04 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef PRINT
+#define PRINT
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int foundString(char*);
+int foundChar(char);
+
+#endif

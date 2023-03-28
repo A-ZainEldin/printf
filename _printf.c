9a10a1821e2707f229bcf5c8ef20293cb9050592
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-28 08:35:30 -0700

    new try

--- _printf.c
+++ _printf.c
@@ -14,34 +14,35 @@ int _printf(const char *format, ...)
 	for (i = 0, sum = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-	{write(1, &format[i], 1);
-	sum++;
-	}
-	else
-	{
-		switch (format[++i])
-	{case 'c':
-		c = (char)va_arg(args, int);
-		write(1, &c, 1);
-		sum++;
-		break;
-	case 's':
-		s = va_arg(args, char *);
-		while (*s != '\0')
-		{write(1, s++, 1);
-		sum++;
-		}
-		break;
-	case '%':
-		write(1, &format[i], 1);
-		sum++;
-		break;
-	default:
-		write(1, &format[i - 1], 2);
-		sum += 2;
-		break;
-	}
-	}
-	} va_end(args);
-	return (sum);
-}
+		{
+			if (format[i] == '\n')
+			{
+				write(1, "\n", 1); }
+			else
+			{write(1, &format[i], 1); }
+			sum++; }
+		else
+		{
+			switch (format[++i])
+			{case 'c':
+					c = (char)va_arg(args, int);
+					write(1, &c, 1);
+					sum++;
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					while (*s != '\0')
+					{
+						write(1, s++, 1);
+						sum++; }
+					break;
+				case '%':
+					write(1, &format[i], 1);
+					sum++;
+					break;
+				default:
+					write(1, &format[i - 1], 2);
+					sum += 2;
+					break;
+			} } } va_end(args);
+	return (sum); }

4e983bb4ba732903424fa76f408c356eba3091b2
A-ZainEldin <ahmedzaineldin5@gmail.com>
2023-03-28 08:22:36 -0700

    new project

--- _printf.c
+++ _printf.c
@@ -1,38 +1,47 @@
 #include "main.h"
 /**
- *_printf- does the job of printf
- *@format: the string passed
- *Return: int of what was printed
+ * _printf - produces output according to a format
+ * @format: character string containing zero or more directives
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, sum;
-
-	if (format == NULL)
-		return (-1);
+	char c, *s;
 
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0, sum = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-		{
-			write(1, &format[i], 1);
-			sum++;
-		}
-		else
-		{
-			if (format[i + 1] == 's')
-			{
-				sum += foundString(va_arg(args, char*));
-				i++;
-			}
-			else if (format[i + 1] == 'c')
-			{
-				sum += foundChar(va_arg(args, int));
-				i++;
-			}
-		}
+	{write(1, &format[i], 1);
+	sum++;
 	}
+	else
+	{
+		switch (format[++i])
+	{case 'c':
+		c = (char)va_arg(args, int);
+		write(1, &c, 1);
+		sum++;
+		break;
+	case 's':
+		s = va_arg(args, char *);
+		while (*s != '\0')
+		{write(1, s++, 1);
+		sum++;
+		}
+		break;
+	case '%':
+		write(1, &format[i], 1);
+		sum++;
+		break;
+	default:
+		write(1, &format[i - 1], 2);
+		sum += 2;
+		break;
+	}
+	}
+	} va_end(args);
 	return (sum);
 }
